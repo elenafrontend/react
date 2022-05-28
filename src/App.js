ec296286010b4f045b9fc32be7e39ce4b3202f55
Elena Gumerova <elenafrontend@ya.ru>
2022-05-28 14:53:39 +0300

    feat: list rendering

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
-import React from 'react';
+import React, {useState} from 'react';
 import "./styles/App.css"
-import PostItem from "./components/PostItem";
+import PostList from "./components/PostList";
 
 function App() {
+  const [posts1, setPosts1] = useState([
+    {id: 1, title: "Javascript", body: "It's description"},
+    {id: 2, title: "Javascript", body: "It's description"},
+    {id: 3, title: "Javascript", body: "It's description"}
+  ]);
+  const [posts2, setPosts2] = useState([
+    {id: 1, title: "Python", body: "It's description"},
+    {id: 2, title: "Python", body: "It's description"},
+    {id: 3, title: "Python", body: "It's description"}
+  ]);
+
   return (
     <div className="App">
-      <PostItem post={{id: 1, title: "Javascript", body: "It's description"}}/>
-      <PostItem post={{id: 2, title: "Javascript", body: "It's description"}}/>
-      <PostItem post={{id: 3, title: "Javascript", body: "It's description"}}/>
-      <PostItem post={{id: 4, title: "Javascript", body: "It's description"}}/>
+      <PostList posts={posts1} title={'Посты про JS'}/>
+      <PostList posts={posts2} title={'Посты про Python'}/>
     </div>
   );
 }
