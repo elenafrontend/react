f0d49b89c7217147c271ef2eae3d0b212ae16022
Elena Gumerova <elenafrontend@ya.ru>
2022-05-28 14:32:05 +0300

    feat: component props

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
-import ClassCounter from "./components/ClassCounter";
+import "./styles/App.css"
+import PostItem from "./components/PostItem";
 
 function App() {
   return (
     <div className="App">
-      <ClassCounter />
+      <PostItem post={{id: 1, title: "Javascript", body: "It's description"}}/>
+      <PostItem post={{id: 2, title: "Javascript", body: "It's description"}}/>
+      <PostItem post={{id: 3, title: "Javascript", body: "It's description"}}/>
+      <PostItem post={{id: 4, title: "Javascript", body: "It's description"}}/>
     </div>
   );
 }
