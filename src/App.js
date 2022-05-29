c8023b29d2146b325101a1b18294122d4cfeae2b
Elena Gumerova <elenafrontend@ya.ru>
2022-05-29 17:53:19 +0300

    feat: limit post count fetching

--- src/App.js
+++ src/App.js
@@ -15,9 +15,14 @@ function App() {
   const [filter, setFilter] = useState({sort: '', query: ''})
   const [modal, setModal] = useState(false);
   const searchedAndSortedPosts = usePosts(posts, filter.sort, filter.query);
+  const [totalCount, setTotalCount] = useState(0);
+  const [limit, setLimit] = useState(10);
+  const [page, setPage] = useState(1);
+
   const [fetchPosts, isPostsLoading, postError] = useFetching(async () => {
-    const posts = await PostService.getAll();
-    setPosts(posts);
+    const response = await PostService.getAll(limit, page);
+    setPosts(response.data);
+    setTotalCount(response.headers['x-total-count']);
   })
 
 
